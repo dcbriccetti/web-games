c1e625c5caac26bf0e627967f6955bca7dbbe656
Dave Briccetti <daveb@davebsoft.com>
2014-07-03 01:30:55 -0700

    Add tweening on menu to animate text

--- invisible-maze/js/menu.js
+++ invisible-maze/js/menu.js
@@ -2,11 +2,14 @@ var menuState = {
     
     create: function() {
         game.stage.backgroundColor = '#00f';
-        var nameLabel = game.add.text(game.world.centerX, 80,
+        var nameLabel = game.add.text(game.world.centerX, -50,
             "Invisible Player Escapes Maze", {
                 font: '50px Arial', fill: '#fff'
             });
         nameLabel.anchor.setTo(0.5, 0.5);
+        var tween = game.add.tween(nameLabel);
+        tween.to({y: 80}, 1000);
+        tween.start();
 
         var startLabel = game.add.text(game.world.centerX, 
             game.world.height - 80,
@@ -14,6 +17,11 @@ var menuState = {
                 font: '25px Arial', fill: '#fff'
             });
         startLabel.anchor.setTo(0.5, 0.5);
+        tween = game.add.tween(startLabel);
+        tween.to({angle: -2}, 500);
+        tween.to({angle: 2}, 500);
+        tween.loop();
+        tween.start();
         
         var space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         space.onDown.addOnce(this.start, this);
