c2f9da297a54c7dbe33057a209ecb5a5057c8509
Dave Briccetti <daveb@davebsoft.com>
2015-02-12 16:36:59 -0800

    Improve code

--- minesweeper-helper/js/mshelper.js
+++ minesweeper-helper/js/mshelper.js
@@ -1,9 +1,8 @@
-"use strict";
-
 var MsHelper = {
     mine: "✸",
-    
-    criteria: [
+    emptyCell: " ",
+
+    cells: [
         "  1     ",
         " 1 2 3  ",
         "        ",
@@ -13,60 +12,44 @@ var MsHelper = {
         "  1 2 3 ",
         "        "
     ],
-    
+
     mapHeight: 0,
     mapWidth:  0,
-    
-    mines: [],
-    
-    emptyCell: " ",
-    
+
     init: function () {
-        this.mapHeight = this.criteria.length,
-        this.mapWidth =  this.criteria[0].length,
-        this.mines = [];
-        for (var i = 0; i < this.mapHeight; ++i) {
-            var row = [];
-            for (var j = 0; j < this.mapWidth; ++j) {
-                row.push(false);
-            }
-            this.mines.push(row);
-        }
+        this.mapHeight = this.cells.length;
+        this.mapWidth =  this.cells[0].length;
     },
-    
+
     build: function () {
         $("#grid tr").remove();
-        var minesPlaced = 0;
-        var numCriteria = 0;
-        var criteriaSatisfied = 0;
-        
-        for (var iRow = 0; iRow < this.mapHeight; ++iRow) {
+        var minesPlaced = 0, numCriteria = 0, criteriaSatisfied = 0, iRow = 0, iCol = 0,
+                cellValue, satisfied, td, thisObject = this;
+
+        function clickFn() {
+            thisObject.clicked(this);
+        }
+
+        for (iRow = 0; iRow < this.mapHeight; ++iRow) {
             var row = $("<tr/>");
-            for (var iCol = 0; iCol < this.mapWidth; ++iCol) {
-                var rv = this.criteria[iRow][iCol];
-                var ch = "";
-                var satisfied = false;
-                if (rv === this.emptyCell) {
-                    var mine = this.mines[iRow][iCol]
-                    if (mine) ++minesPlaced;
-                    ch = mine ? this.mine : this.emptyCell;
-                } else {
+            for (iCol = 0; iCol < this.mapWidth; ++iCol) {
+                cellValue = this.cells[iRow][iCol];
+                satisfied = false;
+                if (cellValue === this.mine) {
+                    ++minesPlaced;
+                } else if (cellValue !== this.emptyCell) {
                     ++numCriteria;
-                    if (parseInt(rv) === this.numNeighboringMines(iRow, iCol)) {
+                    if (parseInt(cellValue) === this.numNeighboringMines(iRow, iCol)) {
                         satisfied = true;
                         ++criteriaSatisfied;
                     }
-                    ch = rv;
                 }
-                var td = $("<td>" + ch + "</td>");
+                td = $("<td>" + cellValue + "</td>");
                 if (satisfied) {
                     td.addClass("satisfied");
                 }
                 td.data("pos", { row: iRow, col: iCol });
-                var msh = this;
-                td.click( function() {
-                    msh.clicked(this);
-                });
+                td.click(clickFn);
                 row.append(td);
             }
             $("#grid").append(row);
@@ -74,45 +57,53 @@ var MsHelper = {
         $("#minesPlaced").text(minesPlaced);
         $("#criteriaSatisfied").text(criteriaSatisfied + " of " + numCriteria);
         $("#finished").text(criteriaSatisfied === numCriteria && minesPlaced === 10 ?
-            "Yes" : "No");
+                "Yes" : "No");
     },
-    
+
     clicked: function (cell) {
+        function replace(cells, row, col, character) {
+            var r = cells[row];
+            cells[row] = r.substr(0, col) + character + r.substr(col + 1);
+        }
         var cellElem = $(cell);
         var cellPos = cellElem.data("pos");
-        var cellValue = this.criteria[cellPos.row][cellPos.col];
+        var cellValue = this.cells[cellPos.row][cellPos.col];
         if (cellValue === this.emptyCell) {
-            var cellText = cellElem.text();
-            var setMine = cellText !== this.mine;
-            cellElem.text(setMine ? this.mine : this.emptyCell);
-            this.mines[cellPos.row][cellPos.col] = setMine;
+            replace(this.cells, cellPos.row, cellPos.col, this.mine);
+            this.build();
+        } else if (cellValue === this.mine) {
+            replace(this.cells, cellPos.row, cellPos.col, this.emptyCell);
             this.build();
         }
     },
-    
-    numNeighboringMines: function (row, col) {
-        var num = 0;
-        var above = row - 1;
-        var below = row + 1;
-        var left  = col - 1;
-        var right = col + 1;
 
-        if (above >= 0) { // Look above unless at top
-            if (left >= 0             && this.mines[above][left ]) ++num;
-            if (                         this.mines[above][col  ]) ++num;
-            if (right < this.mapWidth && this.mines[above][right]) ++num;
+    numNeighboringMines: function (row, col) {
+        var o = this;
+
+        function isMine(row, col) {
+            if (row < 0 || row >= o.mapHeight || col < 0 || col >= o.mapWidth) {
+                return 0;
+            }
+            return o.cells[row][col] === o.mine ? 1 : 0;
         }
-        
-        // Look at same row
-        if (left >= 0             && this.mines[row][left ]) ++num;
-        if (right < this.mapWidth && this.mines[row][right]) ++num;
-        
-        if (below < this.mapHeight) { // Look below unless at bottom
-            if (left >= 0             && this.mines[below][left ]) ++num;
-            if (                         this.mines[below][col  ]) ++num;
-            if (right < this.mapWidth && this.mines[below][right]) ++num;
-        }
-        
-        return num;
+
+        var above = row - 1,
+            below = row + 1,
+            left  = col - 1,
+            right = col + 1;
+
+        return (
+            isMine(above, left ) +
+            isMine(above, col  ) +
+            isMine(above, right) +
+
+            isMine(row,   left ) +
+            // Skip the cell itself
+            isMine(row,   right) +
+
+            isMine(below, left ) +
+            isMine(below, col  ) +
+            isMine(below, right)
+        );
     }
 };
