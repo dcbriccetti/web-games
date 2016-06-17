f4b15a1dacb369f83b369362018dda58a8a4fbe2
Dave Briccetti <daveb@davebsoft.com>
2016-06-17 15:00:07 -0700

    Add walk right and walk left animations to simple game

--- simple-game/game2.js
+++ simple-game/game2.js
@@ -4,8 +4,8 @@ Simple.Game = function () {};
 
 Simple.Game.prototype = {
     preload: function() {
-        this.load.image('dude', 'dude.png');
         this.load.image('enemy', 'enemy.png');
+        this.load.spritesheet('lady', 'lady.png', 50, 100);
     },
 
     player: null,
@@ -16,41 +16,45 @@ Simple.Game.prototype = {
         this.stage.backgroundColor = 'e0e0f0';
         this.physics.startSystem(Phaser.Physics.ARCADE);
 
-        player = this.add.sprite(40, this.world.height / 2, 'dude');
-        enemy  = this.add.sprite(40, 40, 'enemy');
-        this.physics.enable(player, Phaser.Physics.ARCADE);
-        this.physics.enable(enemy,  Phaser.Physics.ARCADE);
-        player.body.drag.set(100);
+        this.player = this.add.sprite(40, this.world.height / 2, 'lady');
+        this.player.animations.add('walk-right', [0, 1]);
+        this.player.animations.add('walk-left',  [2, 3]);
+        this.enemy  = this.add.sprite(40, 40, 'enemy');
+        this.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.physics.enable(this.enemy,  Phaser.Physics.ARCADE);
+        this.player.body.drag.set(100);
 
         cursors = this.input.keyboard.createCursorKeys();
     },
 
     update: function() {
         function spriteContact(obj1, obj2) {
-            player.kill();
+            this.player.kill();
         }
         var playerVelX = 0;
         var playerVelY = 0;
         var vel = 100;
         if (cursors.right.isDown) {
             playerVelX = vel;
+            this.player.animations.play('walk-right', 4);
         } else if (cursors.left.isDown) {
             playerVelX = -vel;
+            this.player.animations.play('walk-left', 4);
         }
         if (cursors.up.isDown) {
             playerVelY = -vel;
         } else if (cursors.down.isDown) {
             playerVelY = vel;
         }
-        player.body.velocity.set(playerVelX, playerVelY);
+        this.player.body.velocity.set(playerVelX, playerVelY);
 
-        var xVel = this.velocityDirection(player.body.position.x, 
-            enemy.body.position.x) * 50;
-        var yVel = this.velocityDirection(player.body.position.y, 
-            enemy.body.position.y) * 50;
-        enemy.body.velocity.set(xVel, yVel);
+        var xVel = this.velocityDirection(this.player.body.position.x, 
+            this.enemy.body.position.x) * 30;
+        var yVel = this.velocityDirection(this.player.body.position.y, 
+            this.enemy.body.position.y) * 30;
+        this.enemy.body.velocity.set(xVel, yVel);
 
-        game.physics.arcade.collide(player, enemy, spriteContact, null, this);
+        game.physics.arcade.collide(this.player, this.enemy, spriteContact, null, this);
     },
     
     velocityDirection: function(val1, val2) {
