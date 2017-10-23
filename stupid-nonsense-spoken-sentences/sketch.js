aadce24c4ca3e2b1f105323eef4aa440f91af3e9
Dave Briccetti <daveb@davebsoft.com>
2017-10-23 14:02:04 -0700

    Move StupidNonsenseSpokenSentences into a class

--- stupid-nonsense-spoken-sentences/sketch.js
+++ stupid-nonsense-spoken-sentences/sketch.js
@@ -1,113 +1,126 @@
-const possibleVoices = 'Alex Alice Anna Carmit Daniel Fiona Fred Karen Kyoko Lekha Maged Mei-Jia Melina Moira Samantha Sin-ji Tessa Ting-Ting Veena Victoria Yuna'.split(' ');
+class StupidNonsenseSpokenSentences {
+    constructor() {
 
-const rec = setUpRecognition();
-const speech = window.speechSynthesis;
-const utterance = new SpeechSynthesisUtterance();
-let voices;
+        this.possibleVoices = 'Alex Alice Anna Carmit Daniel Fiona Fred Karen Kyoko Lekha Maged Mei-Jia Melina Moira Samantha Sin-ji Tessa Ting-Ting Veena Victoria Yuna'.split(' ');
 
-let partIndex = 0;
-const parts = ['adjectives', 'singular nouns', 'indicative verbs', 'adverbs'];
-const adjectives = [];
-const nouns = [];
-const verbs = [];
-const adverbs = [];
-const words = [adjectives, nouns, verbs, adverbs];
+        this.rec = this.setUpRecognition();
+        this.speech = window.speechSynthesis;
+        this.utterance = new SpeechSynthesisUtterance();
+        this.voices = null;
 
-let nonsenseNumber = 0;
+        this.partIndex = 0;
+        this.parts = ['adjectives', 'singular nouns', 'indicative verbs', 'adverbs'];
+        this.adjectives = [];
+        this.nouns = [];
+        this.verbs = [];
+        this.adverbs = [];
+        this.words = [this.adjectives, this.nouns, this.verbs, this.adverbs];
 
-speech.onvoiceschanged = () => {
-    voices = speech.getVoices();
-    giveIntro();
-};
+        this.nonsenseNumber = 0;
 
-function setUpRecognition() {
-    const rec = new webkitSpeechRecognition();
-    rec.onend = () => handleEnd();
-    rec.continuous = true;
-    rec.interimResults = false;
-    rec.onresult = parseResult;
-    return rec;
-}
-
-function giveIntro() {
-    utterance.voice = voice('Daniel');
-    speak('Hi. Please say done after providing words of each type.',
-        promptForWords);
-}
-
-function voice(voiceName) {
-    return voices.filter(v => v.name === voiceName)[0];
-}
-
-function promptForWords() {
-    speak(`Please give me several ${parts[partIndex]}.`,
-        () => rec.start(true, false));
-}
-
-function speakSentence() {
-    const voiceName = randChoice(possibleVoices);
-    let sentence = 'The';
-    words.forEach(wordType => sentence += ' ' + randChoice(wordType));
-    utterance.voice = voice(voiceName);
-    speak(sentence);
-    const p = $('<p>').attr('id', 'nonsense' + nonsenseNumber++);
-    p.append(`${voiceName}: ${sentence}`);
-    p.appendTo('#nonsense');
-    removeOldNonsense();
-}
-
-function removeOldNonsense() {
-    const expiredNum = nonsenseNumber - 6;
-    if (expiredNum >= 0) {
-        const toRemove = $('#nonsense' + expiredNum);
-        toRemove.fadeOut(3000, () => toRemove.remove());
+        this.speech.onvoiceschanged = () => {
+            this.voices = this.speech.getVoices();
+            this.giveIntro();
+        };
     }
-}
 
-function parseResult(e) {
-    if (e.returnValue && e.results.length >= 1) {
-        const input = e.results[e.results.length - 1][0].transcript.trim();
-        const newWords = input.split(' ');
-        newWords.forEach(word => {
-            if (word.toLowerCase() === 'done') {
-                rec.abort();
-                if (++partIndex >= parts.length) {
-                    prepareNonsense();
+    setUpRecognition() {
+        const self = this;
+        const rec = new webkitSpeechRecognition();
+        rec.onend = () => self.handleEnd();
+        rec.continuous = true;
+        rec.interimResults = false;
+        rec.onresult = (event) => self.parseResult(event);
+        return rec;
+    }
+
+    giveIntro() {
+        this.utterance.voice = this.voice('Daniel');
+        const self = this;
+        this.speak('Hi. Please say done after providing words of each type.',
+            () => self.promptForWords());
+    }
+
+    voice(voiceName) {
+        return this.voices.filter(v => v.name === voiceName)[0];
+    }
+
+    promptForWords() {
+        const partType = this.parts[this.partIndex];
+        const self = this;
+        this.speak(`Please give me several ${partType}.`,
+            () => self.rec.start(true, false));
+    }
+
+    speakSentence() {
+        const voiceName = this.randChoice(this.possibleVoices);
+        let sentence = 'The';
+        this.words.forEach(wordType => sentence += ' ' + this.randChoice(wordType));
+        this.utterance.voice = this.voice(voiceName);
+        this.speak(sentence);
+        const p = $('<p>').attr('id', 'nonsense' + this.nonsenseNumber++);
+        p.append(`${voiceName}: ${sentence}`);
+        p.appendTo('#nonsense');
+        this.removeOldNonsense();
+    }
+
+    removeOldNonsense() {
+        const expiredNum = this.nonsenseNumber - 6;
+        if (expiredNum >= 0) {
+            const toRemove = $('#nonsense' + expiredNum);
+            toRemove.fadeOut(3000, () => toRemove.remove());
+        }
+    }
+
+    parseResult(e) {
+        if (e.returnValue && e.results.length >= 1) {
+            const input = e.results[e.results.length - 1][0].transcript.trim();
+            const newWords = input.split(' ');
+            newWords.forEach(word => {
+                if (word.toLowerCase() === 'done') {
+                    this.rec.abort();
+                    if (++this.partIndex >= this.parts.length) {
+                        this.prepareNonsense();
+                    }
+                } else {
+                    this.collectWord(word);
                 }
-            } else {
-                collectWord(word);
-            }
-        });
+            });
+        }
+    }
+
+    collectWord(word) {
+        const pidx = this.partIndex;
+        this.words[pidx].push(word);
+        const dt = $('#' + this.parts[pidx].replace(' ', '-'));
+        dt.append(word + ' ');
+    }
+
+    prepareNonsense() {
+        $('#suggestions').remove();
+        const nh = $('#nonsense-heading');
+        nh.show();
+        const self = this;
+        this.speak(nh.text(), () => setTimeout(() => self.speakSentence(), 1000));
+    }
+
+    handleEnd() {
+        if (this.partIndex < this.parts.length) {
+            this.promptForWords();
+        }
+    }
+
+    speak(message, doAfter) {
+        if (doAfter) {
+            this.utterance.onend = doAfter;
+        }
+        this.utterance.text = message;
+        this.speech.speak(this.utterance);
+    }
+
+    randChoice(sequence) {
+        return sequence[Math.floor(Math.random() * sequence.length)];
     }
 }
 
-function collectWord(word) {
-    words[partIndex].push(word);
-    const dt = $('#' + parts[partIndex].replace(' ', '-'));
-    dt.append(word + ' ');
-}
-
-function prepareNonsense() {
-    $('#suggestions').remove();
-    const nh = $('#nonsense-heading');
-    nh.show();
-    speak(nh.text(), () => setTimeout(speakSentence, 1000));
-}
-
-function handleEnd() {
-    if (partIndex < parts.length) {
-        promptForWords();
-    }
-}
-
-function speak(message, doAfter) {
-    if (doAfter) {
-        utterance.onend = doAfter;
-    }
-    utterance.text = message;
-    speech.speak(utterance);
-}
-
-function randChoice(sequence) {
-    return sequence[Math.floor(Math.random() * sequence.length)];
-}
+new StupidNonsenseSpokenSentences();
