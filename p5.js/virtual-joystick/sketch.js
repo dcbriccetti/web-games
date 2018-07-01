5c390428dfd86ddf53f98cbd40f1bb9410c7a840
Dave Briccetti <daveb@davebsoft.com>
2018-07-01 11:45:10 -0700

    Add left/right movement and lighting

--- p5.js/virtual-joystick/sketch.js
+++ p5.js/virtual-joystick/sketch.js
@@ -1,17 +1,37 @@
+let movable = false;
 
 function setup() {
     const c = createCanvas(400, 400, WEBGL);
     c.parent('#main');
 }
 
+function getDisplacement(halfDistance, mouse) {
+    return constrain(halfDistance - mouse, -halfDistance, halfDistance);
+}
+
 function draw() {
-    background(72, 181, 253);
-    const stickLen = width * 0.4;
-    push();
-    rotateX(PI * 1.5 + sin(frameCount / 15) * PI / 8); // + map(height / 2 - mouseY, 0, height / 2, 0, PI / 8));
+    background(0);
+    const stickLen = width * 0.45;
+    const halfHeight = height / 2;
+    const halfWidth = width / 2;
+    const mouseDisplacementX = getDisplacement(halfWidth, mouseX);
+    const mouseDisplacementY = getDisplacement(halfHeight, mouseY);
+
+    ambientLight(100);
+    directionalLight(255, 255, 255, -width / 4, -height / 4, 0);
+    ambientMaterial(255, 0, 0);
+
+    rotateX(PI * 1.5);
+    if (movable) {
+        rotateX(map(mouseDisplacementY, 0, halfHeight, 0, PI / 8));
+        rotateZ(map(mouseDisplacementX, -halfWidth, halfWidth, PI / 8, -PI / 8));
+    }
     translate(0, -stickLen / 2, 0);
     noStroke();
-    fill(32, 81, 112);
     cylinder(stickLen / 6, stickLen);
-    pop();
+}
+
+function mouseMoved() {
+    if (!movable && dist(mouseX, mouseY, width / 2, height / 2) < width / 4)
+        movable = true;
 }
